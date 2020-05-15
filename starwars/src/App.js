4981600b53a7f104815706174a1e8e482dd2256d
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-15 10:48:36 -0500

    App.js imports and created initital styles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import styled from "styled-components";
+import CharCardCont from "./components/CharacterCardCont";
+
+const HeaderStyle = styled.h1`
+  font-size: 5rem;
+`;
+
+const AppStyle = styled.div`
+  margin-top: 10%;
+`;
+
+const SubTitle = styled.h2`
+  color: #443e3e;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <AppStyle className='App'>
+      <HeaderStyle className='Header'>React Wars</HeaderStyle>
+      <SubTitle>Character List</SubTitle>
+
+      <CharCardCont />
+    </AppStyle>
   );
-}
+};
 
 export default App;
